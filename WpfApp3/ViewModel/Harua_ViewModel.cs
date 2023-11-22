f036dde2755c3c1b799e08562dedbf05ef8c369a
sheephuman <shin.citysheep@gmail.com>
2023-11-23 01:28:31 +0900

    Interfaceを使用したイベント処理に修正。利点がよく分からん。

--- WpfApp3/ViewModel/Harua_ViewModel.cs
+++ WpfApp3/ViewModel/Harua_ViewModel.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using WpfApp3.Parameter;
 
 namespace HaruaConvert.Parameter
