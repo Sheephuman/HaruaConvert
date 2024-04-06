efbc0f246a94cf4e76eefef1e729bbbd98e6f4b3
sheephuman <shin.citysheep@gmail.com>
2024-04-06 17:34:08 +0900

    ffmpegからCodec名を取得する処理を追加

--- WpfApp3/QueryBuildwindow/QueryBuildwindow,cs.xaml.cs
+++ WpfApp3/QueryBuildwindow/QueryBuildwindow,cs.xaml.cs
@@ -1,5 +1,9 @@
-﻿using HaruaConvert.QueryBuildwindow.GetCodecs;
+﻿using HaruaConvert.QueryBuildwindow;
+using HaruaConvert.QueryBuildwindow.GetCodecs;
 using HaruaConvert.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,20 +17,47 @@ namespace HaruaConvert.userintarface
     public partial class QueryCreateWindow : Window
     {
         readonly int minValue = 500;
-    
-  readonly int maxValue = 2000;
+
+
+        public Dictionary<string, string> FfmpegVideoCodecDic { get; set;}
+        public Dictionary<string, string> FfmpegAudioCodecDic { get; set; }
+
+        readonly int maxValue = 2000;
         
         public QueryCreateWindow(WpfNumericUpDown wp)
         {
 
 
             InitializeComponent();
+
+            var getCoudecs = new GetCodecsName();
+
+            var codec = new CodecTypeManager();
+
+
+            FfmpegVideoCodecDic = getCoudecs.GetCodecNameExecute(codec.TypeVideo);
+
+             FfmpegAudioCodecDic = getCoudecs.GetCodecNameExecute(codec.TypeAudio);
+
+            //foreach (var cocec in FfmpegAudioCodecDic)
+            //{ Debug.WriteLine(cocec); }
+
+              DataContext = this;
+            wp.TheNUDTextBox.Text = minValue.ToString(CultureInfo.CurrentCulture);
+
+
+            FileNameExtentionBox.Items.Add(".mp4");
+            FileNameExtentionBox.Items.Add(".avi");
+            FileNameExtentionBox.Items.Add(".gif");
+            FileNameExtentionBox.Items.Add(".wmv");
+            FileNameExtentionBox.Items.Add(".mov");            
+            FileNameExtentionBox.Items.Add(".mkv");
+            FileNameExtentionBox.Items.Add(".flv:");
+            FileNameExtentionBox.Items.Add(".webm");
+            FileNameExtentionBox.Items.Add(".mpeg");
+            FileNameExtentionBox.Items.Add(".rmvb");                        
             
 
-            wp.TheNUDTextBox.Text = minValue.ToString(CultureInfo.CurrentCulture);
-            //wp.TheNUDTextBox.TextChanged += QueryBuildChanged;
-          // wp.NUDTextBox.LostFocus += NUDTextBox_LostFocus;
-//            textbox.TextChanged += QueryBuildChanged; 
         }
 
        
@@ -38,14 +69,11 @@ namespace HaruaConvert.userintarface
 
         private void GetVideoCodecsButton_Click(object sender, RoutedEventArgs e)
         {
-            var getCoudecs = new GetCodecsName();
-            
-            var resultList = getCoudecs.GetCodecNameExecute();
-            foreach (var colist in resultList )
-                if(!VideoCodecBox.Items.Contains(colist))
-                     VideoCodecBox.Items.Add( colist );
+            //foreach (var colist in resultList )
+            //    if(!VideoCodecBox.Items.Contains(colist))
+            //         VideoCodecBox.Items.Add( colist );
 
-
+               
         }
     }
 }
