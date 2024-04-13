0882830b93594b39517bcc46c0c23a5b2ffe7400
sheephuman <shin.citysheep@gmail.com>
2024-04-13 20:01:34 +0900

    Assemblyinfoに CA1016,CA1824対応 Versionと言語を設定

--- WpfApp3/AssemblyInfo.cs
+++ WpfApp3/AssemblyInfo.cs
@@ -1,5 +1,9 @@
+using System.Reflection;
 using System.Windows;
+using System.Resources;
 
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("ja-JP")]
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
--- WpfApp3/Command/QuerryBuildManager.cs
+++ WpfApp3/Command/QuerryBuildManager.cs
@@ -1,11 +1,5 @@
 ﻿using HaruaConvert.UserControls;
 using HaruaConvert.userintarface;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
 
 namespace HaruaConvert.Command
