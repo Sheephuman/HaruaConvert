7ad607d84250a5c8290aa782a26ddb73c97b12e0
sheephuman <shin.citysheep@gmail.com>
2024-04-18 00:20:39 +0900

    QuearyBuildHelper追加

---
+++ WpfApp3/QueryBuilder/QuearyBuildHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaruaConvert.QueryBuilder
+{
+    public class QuearyBuildHelper
+    {
+        public QuearyBuildHelper(QueryField _qf) {
+        
+        qf = _qf;
+        }
+
+       
+
+        QueryField qf;
+
+        public string QueryBuilderExecute(string bit)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format(CultureInfo.CurrentCulture, $"-b:v {bit}k "));
+            
+            
+
+            return sb.ToString(); 
+        }
+
+    }
+}
