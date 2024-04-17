70e26e70b89a3e84ed2b424ee825a94b8463f457
sheephuman <shin.citysheep@gmail.com>
2024-04-18 00:19:35 +0900

    コーデックリストの取得処理追加

---
+++ WpfApp3/QueryBuilder/GetCodecs/GetCodecsName.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Documents;
+
+namespace HaruaConvert.QueryBuilder.GetCodecs
+{
+    public class GetCodecsName
+    {
+        public GetCodecsName() { }
+
+
+        public GetCodecsName(string name) { }
+
+        public Dictionary<string,string> GetCodecNameExecute(string codecType)
+        {
+            var lineDic = new Dictionary<string,string>();
+            // ffmpegのパスを設定
+            var ffmpegPath = @"dll\\ffmpeg.exe";
+            var startInfo = new ProcessStartInfo(ffmpegPath, "-encoders")
+            {
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+
+            var process = new Process { StartInfo = startInfo };
+            process.Start();
+
+           var regex = new Regex(codecType);
+
+///     var regex = new Regex(@"^\s*V\s*\.\.\.\.\.\s+([^\s]+)");
+              var outregex = new Regex(@"V\.\.\.\.\.\s*=\s*(\S+)");
+          
+
+
+            bool isFirstLine =true ;
+
+
+            using (var reader = process.StandardOutput)
+            {
+                string line;
+
+                
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains('='))
+                        continue;
+
+                    //一行目判定
+                    if (isFirstLine)
+                    { 
+                    isFirstLine = false;
+                        continue;
+                    }
+
+
+                    var outMatch = outregex.Match(line);
+                 
+                    var match = regex.Match(line);
+                    if(!outMatch.Success)                     
+                    if (match.Success)
+                    {
+                           
+
+                            //CA1310対応
+                            int startIndex = 7;
+
+                            line = line.TrimStart();
+                            var analizeSorce = line.Substring(startIndex);
+
+                            int startIndex2 = analizeSorce.IndexOf(" ", StringComparison.OrdinalIgnoreCase);
+                            var codecName = analizeSorce.Remove(startIndex2);
+                            var codecDoc = codecName + " : " +
+                                 analizeSorce.Substring(startIndex2).TrimStart();
+                            
+
+                           
+
+
+                            if(!lineDic.ContainsKey(codecName))
+                                  lineDic.Add(codecDoc, codecName);
+                    }
+                }
+            }
+
+            process.WaitForExit();
+
+            return lineDic;
+        }
+
+
+    }
+
+}
