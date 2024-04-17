8fc4abd0448798b011465f7d05ac03df73aedd88
sheephuman <shin.citysheep@gmail.com>
2024-04-18 00:17:07 +0900

    QueryField新設

---
+++ WpfApp3/QueryBuilder/QueryField.cs
@@ -0,0 +1,57 @@
+﻿    using System;
+    using System.ComponentModel;
+using System.Diagnostics;
+
+namespace HaruaConvert.QueryBuilder
+    {
+        public class QueryField: INotifyPropertyChanged
+        {
+
+         private string _bitrateQuery;
+        public string BitRateQuery
+        {  get => _bitrateQuery;
+            
+            set
+            { 
+                if (value == null)
+                    if(_bitrateQuery != value)
+                _bitrateQuery = value;
+                    OnPropertyChanged(nameof(BitRateQuery));
+            
+            }
+                }
+
+
+            private  string _buildQueryes;
+
+            public string BuildQueryes
+            {
+                get { return _buildQueryes; }
+                set { 
+
+                    if (string.IsNullOrEmpty(value))
+                        throw new ArgumentNullException(value);
+                if (_buildQueryes != value)
+                {
+                    _buildQueryes = value;
+                    OnPropertyChanged(nameof(BuildQueryes));
+                }
+             
+            }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                // プロパティ名が空またはnullの場合は例外を投げる。
+                if (string.IsNullOrEmpty(propertyName)) throw new ArgumentNullException(nameof(propertyName));
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+            }
+
+     
+        }
+    }
