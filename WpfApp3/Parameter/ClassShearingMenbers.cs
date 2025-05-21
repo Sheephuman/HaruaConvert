690631c3c408f713dd137c435d8e3e78d294f091
shintarou yamasaki <shin.citysheep@gmail.com>
2025-05-22 00:38:48 +0900

    .net9.0に変更 拡張パラメータ内の変数をローカル変数に変更 クエリの修正

--- WpfApp3/Parameter/ClassShearingMenbers.cs
+++ WpfApp3/Parameter/ClassShearingMenbers.cs
@@ -1,15 +1,8 @@
-﻿using FFMpegCore.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WpfApp3.Parameter
+﻿namespace WpfApp3.Parameter
 {
     public static class ClassShearingMenbers
     {
-        internal static string defaultQuery = "-codec:v libx265 -vf yadif = 0:-1:1 -pix_fmt yuv420p -acodec aac -threads 2";
+        internal static string defaultQuery = "-codec:v libx265 -vf yadif=0:-1:1 -pix_fmt yuv420p -acodec aac -threads 2";
 
         public static string ButtonName { get; set; }
 
