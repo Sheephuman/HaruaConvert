4726904fb390d1e40a4b316b2ddc93912b03e3fb
shintarou yamasaki <shin.citysheep@gmail.com>
2025-07-02 10:50:39 +0900

    WpfNumericUpDown.xaml.cs内 ボタン処理が正常に動作していなかったのを修正

--- WpfApp3/UserControls/WpfNumericUpDown.xaml.cs
+++ WpfApp3/UserControls/WpfNumericUpDown.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -12,30 +11,30 @@ namespace HaruaConvert.UserControls
     /// </summary>
     public partial class WpfNumericUpDown : UserControl
     {
-      static bool isfirst { get; set; }
+        static bool isfirst { get; set; }
         /// <summary>
         /// https://stackoverflow.com/questions/841293/where-is-the-wpf-numeric-updown-control
         /// からの流用
         /// https://github.com/Torabi/WPFNumericUpDown　これも似たようなもの？
         /// </summary>
         /// 
-       
 
-            public WpfNumericUpDown()
+
+        public WpfNumericUpDown()
         {
             InitializeComponent();
-            
-                //
-                //
-                //
-                //TextBox.Text = minvalue.ToString(CultureInfo.CurrentCulture);
 
-                isfirst = true;
-            
-            nuManager = new NumericUpDownManager( NUDTextBox);
+            //
+            //
+            //
+            //TextBox.Text = minvalue.ToString(CultureInfo.CurrentCulture);
+
+            isfirst = true;
+
+            nuManager = new NumericUpDownManager(NUDTextBox);
         }
 
-      public static MainWindow main { get; set; }
+        public static MainWindow main { get; set; }
 
         NumericUpDownManager nuManager;
 
@@ -43,14 +42,14 @@ namespace HaruaConvert.UserControls
         {
             InitializeComponent();
             selGenerate = _selG;
-            if(selGenerate >= 500)
+            if (selGenerate >= 500)
                 TheNUDTextBox.Text = _selG.ToString(CultureInfo.CurrentCulture);
         }
 
-
+        //ParamSelector生成数の最小値と最大値
         private readonly int minvalue = 1;
         private readonly int maxvalue = 100;
-       
+
         public int selGenerate { get; set; }
 
 
@@ -68,29 +67,29 @@ namespace HaruaConvert.UserControls
         /// <param name="e"></param>
         public void NUDTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-         
+
         }
 
         public void NUDButtonDown_Click(object sender, RoutedEventArgs e)
         {
-            
-           nuManager.NUDButtonDown(NUDTextBox,minvalue, -1);
+
+            nuManager.NUDButtonDown(NUDTextBox, minvalue, -1);
         }
 
         public void NUDTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
         {
-           nuManager.NUDTextBox_PreviewKeyDownProc(NUDTextBox, minvalue, maxvalue,-1, e);
+            nuManager.NUDTextBox_PreviewKeyDownProc(NUDTextBox, minvalue, maxvalue, -1, e);
         }
 
         public void NUDTextBox_PreviewKeyUp(object sender, KeyEventArgs e)
         {
-             nuManager.NUDTextBox_PreviewKeyDownProc(NUDTextBox, minvalue, maxvalue,-1,  e);
+            nuManager.NUDTextBox_PreviewKeyDownProc(NUDTextBox, minvalue, maxvalue, -1, e);
         }
 
         public void NUDButtonUP_Click(object sender, RoutedEventArgs e)
         {
-            
-            nuManager.NUDButtonUP_ClickProc(NUDTextBox, maxvalue,1);
+
+            nuManager.NUDButtonUP(NUDTextBox, maxvalue, 1);
         }
 
         public static explicit operator WpfNumericUpDown(QueryCreateUpDown v)
@@ -100,7 +99,7 @@ namespace HaruaConvert.UserControls
 
         private void NUDTextBox_LostFocus(object sender, RoutedEventArgs e)
         {
-            nuManager.ValidateAndCorrectInput(NUDTextBox,minvalue,maxvalue);
+            nuManager.ValidateAndCorrectInput(NUDTextBox, minvalue, maxvalue);
             // フォーカスが外れた時に入力を検証し、必要に応じて修正
         }
     }
