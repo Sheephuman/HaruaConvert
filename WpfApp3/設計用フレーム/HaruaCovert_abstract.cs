dfef01d49d73060d546125ca9959fefa93a01d45
sheeohuman <shin.citysheep@gmail.com>
2026-01-18 19:09:45 +0900

    MSIXの追加

---
+++ WpfApp3/設計用フレーム/HaruaCovert_abstract.cs
@@ -0,0 +1,23 @@
+﻿namespace HaruaConvert.設計用フレーム
+{
+
+
+    /// <summary>
+    /// 最小限の機能を持つ基底クラス
+    /// MainWindowを想定
+    /// 1.ffmpegの入出力
+    /// 2.ファイルの読み込み
+    /// </summary>
+    abstract class HaruaCovert_abstract
+    {
+        string ffmpegQuery { get; set; }
+        bool isFfmpegExcecuted { get; set; } = true;
+
+        string endStrings { get; set; }
+
+
+        public abstract void LoadFile();
+
+        public abstract void CommonOpenDialog();
+    }
+}
