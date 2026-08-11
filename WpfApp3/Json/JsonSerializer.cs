52d7817b7f8fd9755d8a5638dd76c4e8e0a7546e
sheephuman <shin.citysheep@gmail.com>
2026-08-11 19:03:29 +0900

    ReadtoRulesQuery　追加。　 読み込み型で整合性を取り保存。 現状、未使用。

--- WpfApp3/Json/JsonSerializer.cs
+++ WpfApp3/Json/JsonSerializer.cs
@@ -1,5 +1,7 @@
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 
 namespace HaruaConvert.Json
@@ -14,12 +16,44 @@ namespace HaruaConvert.Json
         }
 
 
-        public List<string> ReadtoJsonFile<T>(string filePath)
+        public Dictionary<string, QueryCheckRules> ReadtoRulesQuery(string filePath)
+        {
+            var jsonData = File.ReadAllText(filePath);
+
+            return JsonConvert.DeserializeObject<
+                Dictionary<string, QueryCheckRules>>(jsonData)
+                ?? new Dictionary<string, QueryCheckRules>();
+        }
+
+
+        public void ReadtoRulesQuery(string filePath, out Dictionary<string, QueryCheckRules> rules)
+        {
+            rules = new Dictionary<string, QueryCheckRules>();
+
+          
+
+            var jsonData = File.ReadAllText(filePath);
+
+            Debug.WriteLine($"Read JSON: {jsonData}");
+            Debug.WriteLine($"Path: {filePath}");
+
+            var ruleConfigs =
+                JsonConvert.DeserializeObject<Dictionary<string, QueryCheckRules>>(jsonData);
+
+            foreach (var rule in ruleConfigs.Keys)
+            {
+                rules.Add(rule, ruleConfigs[rule]);
+            }
+        }
+
+        public List<string> ReadtoCommandHistory<T>(string filePath)
         {
             List<string> tokens = new();
 
             string args = string.Empty;
             var jsonData = File.ReadAllText(filePath);
+            Debug.WriteLine($"Read JSON: {jsonData}");
+            Debug.WriteLine($"Path: {filePath}");
 
             var qHistory = JsonConvert.DeserializeObject<CommandHistory>(jsonData);
 
